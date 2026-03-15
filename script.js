84ad0a839cb84623cbd1fcc88a3809212a47b275
BryceSant <161985646+BryceSant@users.noreply.github.com>
2026-03-15 16:12:53 -0700

    completed the main parts of the program! Just need to do flexbox.

---
+++ script.js
@@ -0,0 +1,36 @@
+let bigBox = document.querySelector("#overall-box");
+const colours = ["red", "blue", "violet", "pink", "yellow", "orange", "green"]
+makeBoxes(16);
+
+function makeBoxes(num) {
+    for (let x = 0; x < num; x++) {
+        const divContainer = document.createElement("div");
+
+        for(let i = 0; i < num; i++) {
+            let rectangleDiv = document.createElement("div");
+            rectangleDiv.style["opacity"] = "0.0";
+            rectangleDiv.style["border-width"] = "0.05rem";
+            rectangleDiv.style["border-style"] = "solid";
+            rectangleDiv.style["width"] = `50px`;
+            rectangleDiv.style["height"] = "50px";
+
+            rectangleDiv.addEventListener("mouseenter", (event) => {
+                event.preventDefault();
+                rectangleDiv.style["background-color"] = colours[Math.floor(Math.random() * colours.length)];
+                console.log(rectangleDiv.style["opacity"])
+                if (parseFloat(rectangleDiv.style["opacity"]) !== 1.0) {
+                    rectangleDiv.style["opacity"] = (parseFloat(rectangleDiv.style["opacity"]) + 0.1).toString();
+                }
+                console.log(rectangleDiv.style["opacity"])
+                
+            });
+
+            divContainer.appendChild(rectangleDiv);
+
+        }
+        bigBox.appendChild(divContainer);
+    }
+
+};
+
+
