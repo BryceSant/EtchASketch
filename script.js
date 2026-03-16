56a48cbc419525b2452173abb817384258e99bca
BryceSant <161985646+BryceSant@users.noreply.github.com>
2026-03-16 12:57:21 -0700

    made vh smaller, and optimized with just one event Listener with mouseover for the bigBox

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ let bigBox = document.querySelector("#overall-box");
 const colours = ["red", "blue", "violet", "pink", "yellow", "orange", "green"]
 makeBoxes(20);
 
+bigBox.addEventListener("mouseover", (event) => {
+    event.preventDefault();
+    let target = event.target;
+    target.style["background-color"] = colours[Math.floor(Math.random() * colours.length)];
+    console.log(target.style["opacity"])
+    if (parseFloat(target.style["opacity"]) !== 1.0) {
+        target.style["opacity"] = (parseFloat(target.style["opacity"]) + 0.1).toString();
+    }
+    console.log(target.style["opacity"])
+    
+
+
+
+});
+
 function makeBoxes(num) {
     for (let x = 0; x < num; x++) {
         const divContainer = document.createElement("div");
@@ -14,17 +29,6 @@ function makeBoxes(num) {
             rectangleDiv.classList.add("squares");
             rectangleDiv.style["width"] = `100%`
             rectangleDiv.style["height"] = `${100/num}%`
-
-            rectangleDiv.addEventListener("mouseenter", (event) => {
-                event.preventDefault();
-                rectangleDiv.style["background-color"] = colours[Math.floor(Math.random() * colours.length)];
-                console.log(rectangleDiv.style["opacity"])
-                if (parseFloat(rectangleDiv.style["opacity"]) !== 1.0) {
-                    rectangleDiv.style["opacity"] = (parseFloat(rectangleDiv.style["opacity"]) + 0.1).toString();
-                }
-                console.log(rectangleDiv.style["opacity"])
-                
-            });
 
             divContainer.appendChild(rectangleDiv);
 
